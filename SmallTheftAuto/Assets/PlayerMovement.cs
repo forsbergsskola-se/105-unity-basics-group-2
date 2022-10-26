5ba1cddec9a8fd2c9eb7ae73e7d562ee2add909a
Sofie Persson <sofie.persson@student.forsbergsskola.se>
2022-10-26 20:11:31 +0200

    Adds Time.deltaTime to Movement

--- SmallTheftAuto/Assets/PlayerMovement.cs
+++ SmallTheftAuto/Assets/PlayerMovement.cs
@@ -1,37 +1,32 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-    
     void Update()
     {
         // only, if the W-Key is currently pressed...
         if (Input.GetKey(KeyCode.W)) {
             // translate the player's transform-component on the y-axis (which points up)
-            transform.Translate(0f, 0.01f, 0f);
+            transform.Translate(0f, 2f * Time.deltaTime, 0f);
         }
         
         // only, if the S-Key is currently pressed...
         if (Input.GetKey(KeyCode.S)) {
             // translate the player's transform-component on the y-axis
-            transform.Translate(0f, -0.01f, 0f);
+            transform.Translate(0f, -2f * Time.deltaTime, 0f);
         }
 
         if (Input.GetKey(KeyCode.A))
         {
-            transform.Rotate(0f, 0f, 0.1f);
+            transform.Rotate(0f, 0f, 40f * Time.deltaTime);
         }
         
         if (Input.GetKey(KeyCode.D))
         {
-            transform.Rotate(0f, 0f, -0.1f);
+            transform.Rotate(0f, 0f, -40f * Time.deltaTime);
         }
     }
 }
