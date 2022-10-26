010a62ac85e75a874eb90380cae453fd4de59cf9
hildurhanna <112477585+hildurhanna@users.noreply.github.com>
2022-10-26 15:40:20 +0200

    Fixed Drag 2.3

--- SmallTheftAuto/Assets/Scripts/Brick.cs
+++ SmallTheftAuto/Assets/Scripts/Brick.cs
@@ -1,45 +1,36 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using Random = UnityEngine.Random;
 
-public class Brick : MonoBehaviour, Brick.IBeginDragHandler, Brick.IDragHandler, Brick.IEndDragHandler
+public class Brick : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
 {
-    
     void Awake()
     {
         transform.position += transform.right * Random.Range(-.1f, .1f);
         
     }
-    public interface IBeginDragHandler
+   
+    public void OnDrag(PointerEventData eventData)
     {
-        public void OnBeginDrag(PointerEventData eventData)
+        Plane plane = new Plane(Vector3.up, Vector3.up * eventData.pointerPressRaycast.worldPosition.y);
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (plane.Raycast(ray, out float distance))
         {
-            GetComponent<Rigidbody>().isKinematic = true;
+            transform.position = ray.GetPoint(distance);
         }
     }
 
-    public interface IDragHandler 
+    public void OnBeginDrag(PointerEventData eventData)
     {
-        public void OnDrag(PointerEventData eventData)
-        {
-            Plane plane = new Plane(Vector3.up, Vector3.up * eventData.pointerPressRaycast.worldPosition.y);
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            if (plane.Raycast(ray, out float distance))
-            {
-                transfrom.position = ray.GetPoint(distance);
-            }
-        }
+        GetComponent<Rigidbody>().isKinematic = true;
     }
 
-    public interface IEndDragHandler 
+    public void OnEndDrag(PointerEventData eventData)
     {
-        public void OnEndDrag(PointerEventData eventData)
-        {
-            GetComponent<Rigidbody>().isKinematic = false;
-        }
+        GetComponent<Rigidbody>().isKinematic = false;
     }
-    
 }
