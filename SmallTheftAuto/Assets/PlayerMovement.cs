c93e92d49471343b21f608d1e166dbd430f99fdb
Sofie Persson <sofie.persson@student.forsbergsskola.se>
2022-10-26 20:24:43 +0200

    Replaces If Statements by Using Input Manager

--- SmallTheftAuto/Assets/PlayerMovement.cs
+++ SmallTheftAuto/Assets/PlayerMovement.cs
@@ -5,28 +5,16 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    public float movementSpeed = 5f;
+    public float rotationSpeed = 40f;
     void Update()
     {
         // only, if the W-Key is currently pressed...
-        if (Input.GetKey(KeyCode.W)) {
-            // translate the player's transform-component on the y-axis (which points up)
-            transform.Translate(0f, 2f * Time.deltaTime, 0f);
-        }
-        
-        // only, if the S-Key is currently pressed...
-        if (Input.GetKey(KeyCode.S)) {
-            // translate the player's transform-component on the y-axis
-            transform.Translate(0f, -2f * Time.deltaTime, 0f);
-        }
+        float movement = Input.GetAxis ("Vertical"); 
+        // translate the player's transform-component on the y-axis (which points up)
+        transform.Translate(0f, movementSpeed * movement * Time.deltaTime, 0f);
 
-        if (Input.GetKey(KeyCode.A))
-        {
-            transform.Rotate(0f, 0f, 40f * Time.deltaTime);
-        }
-        
-        if (Input.GetKey(KeyCode.D))
-        {
-            transform.Rotate(0f, 0f, -40f * Time.deltaTime);
-        }
+        float rotation = Input.GetAxis("Horizontal");
+        transform.Rotate(0f,0f,-rotationSpeed * rotation * Time.deltaTime);
     }
 }
