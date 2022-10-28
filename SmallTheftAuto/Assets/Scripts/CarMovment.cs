15829370240c9ec2169e4545f0cbcfef27cc7d57
hildurhanna <112477585+hildurhanna@users.noreply.github.com>
2022-10-28 10:06:14 +0200

    Added CarMovement Script

---
+++ SmallTheftAuto/Assets/Scripts/CarMovment.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarMovment : MonoBehaviour
+{
+    public float speed = 10.0f;
+    public float rotationSpeed = 100.0f;
+
+    
+    void Update()
+    {
+        float translation = Input.GetAxis("Vertical") * speed;
+        float rotation = Input.GetAxis("Horizontal") * rotationSpeed;
+        translation *= Time.deltaTime;
+        rotation *= Time.deltaTime;
+        transform.Translate(translation,0, 0);
+        transform.Rotate(0, rotation, 0);
+    }
+}
