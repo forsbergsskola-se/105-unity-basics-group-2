06b31000bdd0478e6a4c867ccdff42f06b195b45
Sofie Persson <sofie.persson@student.forsbergsskola.se>
2022-10-30 19:12:02 +0100

    Creates Car GameObject and CarMovement Script

---
+++ SmallTheftAuto/Assets/CarMovement.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarMovement : MonoBehaviour
+{
+    public float movementSpeed = 5f;
+    public float rotationSpeed = 40f;
+    void Update()
+    {
+        // only, if the W-Key is currently pressed...
+        float movement = Input.GetAxis ("Vertical"); 
+        // translate the player's transform-component on the y-axis (which points up)
+        transform.Translate(0f, movementSpeed * movement * Time.deltaTime, 0f);
+
+        float rotation = Input.GetAxis("Horizontal");
+        transform.Rotate(0f,0f,-rotationSpeed * rotation * Time.deltaTime);
+    }
+}
