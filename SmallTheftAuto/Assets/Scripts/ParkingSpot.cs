380cf9074750218875cf12aa40b6a79250cdab82
Sofie Persson <sofie.persson@student.forsbergsskola.se>
2022-10-31 13:18:19 +0100

    Adds ParkingSpot Script

--- SmallTheftAuto/Assets/Scripts/ParkingSpot.cs
+++ SmallTheftAuto/Assets/Scripts/ParkingSpot.cs
@@ -4,10 +4,17 @@ using UnityEngine;
 
 public class ParkingSpot : MonoBehaviour
 {
+    public bool hasCar;
+
+    public GameObject carPrefab;
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (hasCar)
+        {
+            var position = transform.position;
+            Instantiate(carPrefab, new Vector3(position.x, position.y, position.x), Quaternion.Euler(0, 0, 0));
+        }
     }
 
     // Update is called once per frame
