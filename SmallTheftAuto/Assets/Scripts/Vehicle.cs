106ca2f5927ec66c50f0b38e5922aed37fa2a204
Sofie Persson <sofie.persson@student.forsbergsskola.se>
2022-10-31 15:27:29 +0100

    Fixes Player Being Able to Enter and Leave Car Created From Parking Spot

--- SmallTheftAuto/Assets/Scripts/Vehicle.cs
+++ SmallTheftAuto/Assets/Scripts/Vehicle.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,6 +8,11 @@ public class Vehicle : MonoBehaviour
     public GameObject player;
 
     public CarMovement carMovement;
+
+    private void Awake()
+    {
+        player = GameObject.Find("Player");
+    }
 
     void Update()
     {
