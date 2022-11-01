548acd6f1befeef79b5e46ba50c3b7a93e97e8e8
hildurhanna <112477585+hildurhanna@users.noreply.github.com>
2022-11-01 11:17:37 +0100

    Added Button Functions

--- SmallTheftAuto/Assets/Scripts/StartGame.cs
+++ SmallTheftAuto/Assets/Scripts/StartGame.cs
@@ -5,8 +5,19 @@ using UnityEngine.SceneManagement;
 
 public class StartGame : MonoBehaviour
 {
+    public GameObject MainMenu;
     public void LoadGame()
     {
         SceneManager.LoadScene("SanAndreas");
+    }
+
+    public void CloseGame()
+    {
+        MainMenu.SetActive(false);
+    }
+
+    public void OpenGame()
+    {
+        MainMenu.SetActive(true);
     }
 }
