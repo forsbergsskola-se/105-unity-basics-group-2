008788246341ae7ba7e1b4bfff3695a9f1552d7f
Sofie Persson <sofie.persson@student.forsbergsskola.se>
2022-11-02 13:57:34 +0100

    Adds Player Stats Scriptable object and organizes scripts

---
+++ SmallTheftAuto/Assets/Scripts/PlayerStats.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu]
+public class PlayerStats : ScriptableObject
+{
+    public int money;
+
+    public int health;
+
+    public int score;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
