efa14a51a0a94bf960ad59812f749efb5501a61e
Sofie Persson <sofie.persson@student.forsbergsskola.se>
2022-11-02 09:04:54 +0100

    Moves LeaveCar Logic to CarMovement Script and Fixes Bugs

--- SmallTheftAuto/Assets/Scripts/CarMovement.cs
+++ SmallTheftAuto/Assets/Scripts/CarMovement.cs
@@ -4,7 +4,6 @@ public class CarMovement : MonoBehaviour
 {
     public float movementSpeed = 5f;
     public float rotationSpeed = 40f;
-    private Driver driver;
     void Update()
     {
         // only, if the W-Key is currently pressed...
@@ -14,20 +13,16 @@ public class CarMovement : MonoBehaviour
 
         float rotation = Input.GetAxis("Horizontal");
         transform.Rotate(0f,0f,-rotationSpeed * rotation * Time.deltaTime);
-
-        driver = FindObjectOfType<Driver>();
-        Vehicle vehicle = FindObjectOfType<Vehicle>();
-        if (driver.EnterCarButtonPressed())
+        
+        Vehicle vehicle = GetComponent<Vehicle>();
+        if (EnterCarButtonPressed())
         {
-            if (PlayerIsInCar())
-            {
-                vehicle.LeaveCar();
-            }
+            vehicle.LeaveCar();
         }
     }
     
-    public bool PlayerIsInCar()
+    public bool EnterCarButtonPressed()
     {
-        return !activeInHierarchy;
+        return Input.GetButtonDown("Interact-Vehicle");
     }
 }
--- SmallTheftAuto/Assets/Scripts/Vehicle.cs
+++ SmallTheftAuto/Assets/Scripts/Vehicle.cs
@@ -4,14 +4,16 @@ using UnityEngine;
 public class Vehicle : MonoBehaviour
 {
     public CarMovement carMovement;
+    private GameObject player;
 
     public void EnterCar(GameObject player)
     {
         player.SetActive(false);
         carMovement.enabled = true;
+        this.player = player;
     }
 
-    public void LeaveCar(GameObject player)
+    public void LeaveCar()
     {
         player.transform.position = transform.position;
         player.SetActive(true);
