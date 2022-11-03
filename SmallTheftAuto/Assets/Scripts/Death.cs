d8548499001ca9b53c38fc071761c729b3ef7560
hildurhanna <112477585+hildurhanna@users.noreply.github.com>
2022-11-03 11:32:26 +0100

    Unfinished Water&Death Scripts

---
+++ SmallTheftAuto/Assets/Scripts/Death.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Death : MonoBehaviour
+{
+   public GameObject player;
+   public void OnDeath()
+   {
+      /*if (player.Health<=0)*/
+      {
+         
+      }
+   }
+}
