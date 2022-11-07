46b2ea5a023bad1ee504a02adfffddbd8c7f8cd7
hildurhanna <112477585+hildurhanna@users.noreply.github.com>
2022-11-07 14:27:14 +0100

    Testing Path Follow

---
+++ SmallTheftAuto/Assets/Scripts/PedestrianPath.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PedestrianPath : MonoBehaviour
+{
+    public Transform[] target;
+    public float speed;
+    private int current;
+
+    private void Update()
+    {
+        if (transform.position != target[current].position)
+        {
+            Vector3 pos = Vector3.MoveTowards(transform.position, target[current].position, speed * Time.deltaTime);
+            GetComponent<Rigidbody>().MovePosition(pos);
+        }
+        else current = (current + 1) % target.Length;
+    }
+}
