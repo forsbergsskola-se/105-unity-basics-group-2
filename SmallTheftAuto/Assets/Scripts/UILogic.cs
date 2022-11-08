f3dea02ca0332c0bc982b552c0da85c1401a83fe
Jacob-Roming <jacob.roming@student.forsbergsskola.se>
2022-11-08 10:04:30 +0100

    Adds quests in full

--- SmallTheftAuto/Assets/Scripts/UILogic.cs
+++ SmallTheftAuto/Assets/Scripts/UILogic.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class UILogic : MonoBehaviour
@@ -9,13 +10,19 @@ public class UILogic : MonoBehaviour
     public TMP_Text pointsText;
     public TMP_Text moneyText;
     public TMP_Text deathText;
+    public TMP_Text questStartText;
+    public GameObject questImage;
 
     public PlayerStats PlayerStats;
     // Start is called before the first frame update
     void Start()
     {
         deathText.enabled = false;
-    }
+        questStartText.enabled = false;
+        questImage.SetActive(false);
+        QuestGiverLogic questGiver = FindObjectOfType<QuestGiverLogic>();
+        questGiver.QuestStarted += showQuestInstruction;
+    }   
 
     // Update is called once per frame
     void Update()
@@ -31,5 +38,21 @@ public class UILogic : MonoBehaviour
             deathText.enabled = true;
         }
         
+    }
+
+    public void showQuestInstruction()
+    {
+        StartCoroutine(TemporaryQuestPopUp());
+        Debug.Log("The event worked");
+    }
+    IEnumerator TemporaryQuestPopUp()
+    {
+        questImage.SetActive(true);
+        questStartText.enabled = true;
+        //yield on a new YieldInstruction that waits for 5 seconds.
+        yield return new WaitForSeconds(5);
+        questImage.SetActive(false);
+        questStartText.enabled = false;
+
     }
 }
