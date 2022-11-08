d5a2ca6b1017145002ccb36f88e7ad51603ad61e
hildurhanna <112477585+hildurhanna@users.noreply.github.com>
2022-11-08 11:57:21 +0100

    Fixed FollowPath for Pedestrian/Traffic

--- SmallTheftAuto/Assets/Scripts/FollowPath.cs
+++ SmallTheftAuto/Assets/Scripts/FollowPath.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,13 +8,27 @@ public class FollowPath : MonoBehaviour
     public Transform[] target;
     public float speed;
     private int current;
+    public float distThreshold = 5f;
+    
+    private Vector3 deltaPosition;
+    private float sqrDistToTarget;
+    private float distThresholdSqr;
 
+    private void Start()
+    {
+        distThresholdSqr = distThreshold * distThreshold;
+    }
+
+        
     private void Update()
     {
-        if (transform.position != target[current].position)
+        deltaPosition = target[current].position - transform.position;
+        sqrDistToTarget = deltaPosition.sqrMagnitude;
+        if (  sqrDistToTarget > distThresholdSqr  )
         {
             Vector3 pos = Vector3.MoveTowards(transform.position, target[current].position, speed * Time.deltaTime);
             GetComponent<Rigidbody>().MovePosition(pos);
+            Debug.Log(pos);
         }
         else current = (current + 1) % target.Length;
     }
