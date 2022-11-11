6d75d39bcbc7798720b4329452a29e755c0dd15d
FelixAtForsberg <113030003+FelixAtForsberg@users.noreply.github.com>
2022-11-11 15:45:43 +0100

    New Weapons Added

---
+++ SmallTheftAuto/Assets/Scripts/Weapons/Bullet/Testing_DynBullet.cs
@@ -0,0 +1,11 @@
+// using System;
+// using UnityEngine;
+// using UnityEngine.Pool;
+//
+// public class Bullet : MonoBehaviour
+// {
+//         // Construct Bullet with these components
+//         public static Type[] BulletComponents = { typeof(Bullet) };
+//
+//         private GameObject BulletGO = new GameObject("Bullet", BulletComponents);
+// }
